ae10304829813378c9aa65aff3cd2435217dfbfc
funanshan <2414785575@qq.com>
2025-12-16 10:24:27 +0800

    feat: 修改FloatingTOC显示逻辑

--- src/types/config.ts
+++ src/types/config.ts
@@ -163,7 +163,7 @@ export type SiteConfig = {
 	toc: {
 		enable: boolean;
 		mode: "float" | "sidebar"; // 目录显示模式："float" 悬浮按钮模式，"sidebar" 侧边栏模式
-		depth: 1 | 2 | 3;
+		depth: 1 | 2 | 3 | 4 | 5 | 6; // 目录深度，1-6，1 表示只显示 h1 标题，2 表示显示 h1 和 h2 标题，依此类推
 		useJapaneseBadge?: boolean; // 使用日语假名标记（あいうえお...）代替数字
 	};
 	showCoverInContent: boolean; // 控制文章封面在文章内容页显示的开关
